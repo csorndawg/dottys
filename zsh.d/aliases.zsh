c3994f02583c9d433d98b9e632407809cc27b352
csorndawg <zaccsorny@yahoo.com>
2024-06-25 23:05:03 +0000

    added custom aliases submodule, zshvi-mode plugin, and autosuggest syntax and key bindings

---
+++ zsh.d/aliases.zsh
@@ -0,0 +1,26 @@
+# ========================================================================
+# 	ALIASES
+# ========================================================================
+
+# TODO: move zshrc/bashrc aliases below
+
+## WORKFLOW ALIASES WIP
+alias gs="git status"
+alias gcb="git checkout -b "
+alias gcmn="git checkout main "
+alias gcmt="git commit -m "
+alias gcmn="git checkout main "
+alias grb="git rebase -i HEAD~$1"
+alias grb2='git rebase -i HEAD~'"$1"
+alias ga="git add"
+alias gpl="git pull "
+alias gp="git push "
+alias gpf="git push --force "
+alias gdf="git diff "
+alias gdfm="git diff main"
+alias gdfh="git diff HEAD~1"
+alias gdfs="git diff --staged "
+alias zsrc="source $HOME/.zshrc "
+alias grs="git restore "$1
+alias grss="git restore --staged "
+alias tmls='tmux ls'
